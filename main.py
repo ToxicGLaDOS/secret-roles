ca7a2c2c3007e59f0bc1a3e5766a7aede55ca513
Jeff Smith <toxicglados@gmail.com>
2020-10-20 18:44:30 -0500

    Add cleanup and betting hinting toward problems

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import string
 import time
 
 TOKEN_LENGTH = 20
-TOKEN_DURATION = 60 * 1 # 5 minutes
+TOKEN_DURATION = 60 * 10 # 5 minutes
 CHECKIN_DURATION = 60 * 1 # 1 minutes
 HOSTNAME = cherrypy.url()
 VALID_TOKEN_CHARS = string.ascii_letters + string.digits
@@ -18,16 +18,30 @@ class Token(object):
         self.roles_remaining = roles.copy()
         self.time_created = time.time()
         self.checkins = {}
+        self.checkins_locked = False
+
+    def lock_checkins(self):
+        self.checkins_locked = True
 
     def checkins_complete(self):
-        if len(self.checkins.values()) != len(self.roles):
+        # If the checkins are locked then people should be able to go back to the /join page
+        # and see there role again (as long as they haven't quit the session)
+        # So until the token expires they're checkin should stay locked to this token
+        # and never get removed
+        if not self.checkins_locked:
+            # Filter out any expired checkins
+            self.checkins = {checkin_str: checkin for checkin_str, checkin in self.checkins.items() if
+                             not checkin.is_timedout()}
+
+        if len(self.checkins.values()) < len(self.roles):
             print("Not all players have joined")
             return False
-        for checkin in self.checkins.values():
-            if checkin.is_timedout():
-                print("A checkin is timed out")
-                return False
+        elif len(self.checkins.values()) > len(self.roles):
+            print("More than expected number of players have joined")
+            return False
         print("Checkins complete")
+
+        self.lock_checkins()
         return True
 
     def is_token_expired(self):
@@ -81,6 +95,13 @@ class HelloWorld(object):
         token = Token(token_str, roles)
         return token
 
+    def cleanup_expired_tokens(self):
+        expired_tokens = {token_str: token for token_str, token in self.openTokens.items() if
+                           token.is_token_expired()}
+        print(f"Cleaning up tokens. Expired tokens {expired_tokens}")
+        print(f"len(self.openTokens) = {len(self.openTokens)}")
+        self.openTokens = {token_str: token for token_str, token in self.openTokens.items() if not token.is_token_expired()}
+
     @cherrypy.expose
     def generate(self, roles=None):
         if roles == None:
@@ -97,17 +118,22 @@ class HelloWorld(object):
     @cherrypy.expose
     def checkin(self, token=None):
         if token is None:
-            return '{"everyone_ready": false, "role": "unknown", "status":"token not found"}'
+            return '{"everyone_ready": false, "role": "unknown", "status":"token not found", "hint": "accessing this url manually isn\'t very helpful"}'
         if token not in self.openTokens:
-            return '{"everyone_ready": false, "role": "unknown", "status":"token invalid"}'
+            return '{"everyone_ready": false, "role": "unknown", "status":"token invalid", "hint": "maybe you missed some of the token when copying?"}'
         token_obj = self.openTokens[token]
 
         if token_obj.is_token_expired():
-            return '{"everyone_ready": false, "role": "unknown", "status":"token expired"}'
+            return '{"everyone_ready": false, "role": "unknown", "status":"token expired", "hint": "you took too long to join the lobby :("}'
 
         checkin_id = cherrypy.session.get('checkin_id')
         # If this user doesn't have a checkin_id yet then give them one
         if checkin_id is None:
+            # If the checkins are already done than this lobby is full
+            # Maybe this user quit their session and has opened a new one
+            # If so, too bad :(
+            if token_obj.checkins_complete():
+                return '{"everyone_ready": true, "role": "None", "status":"all roles taken", "hint": "perhaps you closed your browser and tried to reopen a link? (you can only see your role again if you have the same browser session open)"}'
             checkin_id = self.generate_checkin_id()
             cherrypy.session['checkin_id'] = checkin_id
 
@@ -120,9 +146,9 @@ class HelloWorld(object):
 
         # If everyone has checked in then we can return the roles to everyone
         if token_obj.checkins_complete():
-            return f'{{"everyone_ready": true, "role":"{token_obj.get_role(checkin_id)}", "status": "done"}}'
+            return f'{{"everyone_ready": true, "role":"{token_obj.get_role(checkin_id)}", "status": "done", "hint": "none"}}'
 
-        return '{"everyone_ready": false, "role": "unknown", "status": "waiting"}'
+        return '{"everyone_ready": false, "role": "unknown", "status": "waiting", "hint": "none"}'
 
     @cherrypy.expose
     def join(self, token=None):
@@ -145,4 +171,7 @@ conf = {
         'tools.sessions.on': True,
     }
 }
-cherrypy.quickstart(HelloWorld(), '/', conf)
+instance = HelloWorld()
+cleanup = cherrypy.process.plugins.BackgroundTask(5, instance.cleanup_expired_tokens)
+cleanup.start()
+cherrypy.quickstart(instance, '/', conf)
